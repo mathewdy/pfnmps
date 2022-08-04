0080923ad19aede81da2c46b1f99046e20171ea3
Raie01 <ryantecling@gmail.com>
2022-08-04 10:23:04 +0800

    Student info update

--- edit-student-image.php
+++ edit-student-image.php
@@ -10,84 +10,172 @@ include('security-admin.php');
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
+<meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="keywords" content="adminkit, bootstrap, bootstrap 5, admin, dashboard, template, responsive, css, sass, html, theme, front-end, ui kit, web">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link rel="shortcut icon" href="img/icons/icon-48x48.png" />
+    <link rel="canonical" href="https://demo-basic.adminkit.io/" />
+    <link href="admin-template/css/app.css" rel="stylesheet">
+	<link rel="./stylesheet" href="./styles/bootstrap/css/bootstrap.css">
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600&display=swap" rel="stylesheet">
     <title>Document</title>
 </head>
 <body>
-    
+    <div class="wrapper">
+		<nav id="sidebar" class="sidebar js-sidebar">
+			<div class="sidebar-content js-simplebar">
+				<a class="sidebar-brand" href="index.html">
+          <span class="align-middle">Admin</span>
+        </a>
+
+				<ul class="sidebar-nav">
+					<li class="sidebar-header">
+						Pages
+					</li>
+
+					<li class="sidebar-item">
+						<a class="sidebar-link" href="home-admin.php">
+                            <i class="align-middle" data-feather="home"></i> <span class="align-middle">Home</span>
+                         </a>
+					</li>
+
+					<li class="sidebar-item active">
+						<a class="sidebar-link" href="view-students.php">
+                            <i class="align-middle" data-feather="user"></i> <span class="align-middle">Students</span>
+                        </a>
+					</li>
+
+					<li class="sidebar-item">
+						<a class="sidebar-link disabled" href="#">
+                            <i class="align-middle" data-feather="star"></i> <span class="align-middle">Nutritionists</span>
+                        </a>
+					</li>
+
+					<li class="sidebar-item">
+						<a class="sidebar-link" href="add-patient.php">
+                            <i class="align-middle" data-feather="user-plus"></i> <span class="align-middle">Add Patient</span>
+                        </a>
+					</li>
+				
+				</ul>
+				<a class="text-light text-center btn btn-primary" href="logout-admin.php">Logout</a>
+			</div>
+		</nav>
+
+		<div class="main">
+			<nav class="navbar navbar-expand navbar-light navbar-bg">
+				<a class="sidebar-toggle js-sidebar-toggle">
+          <i class="hamburger align-self-center"></i>
+        </a>
+
+				<div class="navbar-collapse collapse">
+					<ul class="navbar-nav navbar-align">
+						
+						<li class="nav-item dropdown">
+							<a class="nav-icon dropdown-toggle d-inline-block d-sm-none" href="#" data-bs-toggle="dropdown">
+                                <i class="align-middle" data-feather="settings"></i>
+                            </a>
+
+							<a class="nav-link dropdown-toggle d-none d-sm-inline-block" href="#" data-bs-toggle="dropdown" id="dropdownMenuButton1">
+                                <span class="text-dark">ADMIN</span>
+                            </a>
+							<div class="dropdown-menu dropdown-menu-end">
+								<a class="dropdown-item" href="pages-profile.html"><i class="align-middle me-1" data-feather="user"></i> Profile</a>
+								<a class="dropdown-item" href="#"><i class="align-middle me-1" data-feather="pie-chart"></i> Analytics</a>
+								<div class="dropdown-divider"></div>
+								<a class="dropdown-item" href="index.html"><i class="align-middle me-1" data-feather="settings"></i> Settings & Privacy</a>
+								<a class="dropdown-item" href="#"><i class="align-middle me-1" data-feather="help-circle"></i> Help Center</a>
+								<div class="dropdown-divider"></div>
+								<a class="dropdown-item" href="logout-admin.php">Log out</a>
+							</div>
+						</li>
+					</ul>
+				</div>
+			</nav>
+
+			<main class="content">
+				<div class="container-fluid p-0">
+                <?php
+
+                    if(isset($_GET['student_id'])){
+                        $student_id = $_GET['student_id'];
+
+                        $query = "SELECT * FROM students WHERE student_id = '$student_id'";
+                        $run = mysqli_query($conn,$query);
+
+                        if(mysqli_num_rows($run) > 0){
+                            foreach($run as $row ){
+                                ?>
 
 
-<?php
+                                    <form action="edit-student-image.php" method="POST"  enctype="multipart/form-data">
 
-if(isset($_GET['student_id'])){
-    $student_id = $_GET['student_id'];
+                                        <a href="edit-student.php?student_id=<?php echo $row ['student_id']?>">Back</a>
 
-    $query = "SELECT * FROM students WHERE student_id = '$student_id'";
-    $run = mysqli_query($conn,$query);
+                                        <label for="">Update Image</label>
+                                        <br>
+                                        <img src="student_image/<?php echo $row ['image']?>" width="200px" height="200px" alt="student image">
+                                        <br>
 
-    if(mysqli_num_rows($run) > 0){
-        foreach($run as $row ){
-            ?>
+                                        <input type="file" name="image">
 
-
-                <form action="edit-student-image.php" method="POST"  enctype="multipart/form-data">
-
-                    <a href="edit-student.php?student_id=<?php echo $row ['student_id']?>">Back</a>
-
-                    <label for="">Update Image</label>
-                    <br>
-                    <img src="student_image/<?php echo $row ['image']?>" width="200px" height="200px" alt="student image">
-                    <br>
-
-                    <input type="file" name="image">
-
-                    <br>
-                    <input type="submit" name="update" value="Update">
-                    <input type="hidden" name="student_id" value="<?php echo $row ['student_id']?>">
-                </form>
+                                        <br>
+                                        <input type="submit" name="update" value="Update">
+                                        <input type="hidden" name="student_id" value="<?php echo $row ['student_id']?>">
+                                    </form>
 
 
-            <?php   
-        }
-    }
-}
+                                <?php   
+                            }
+                        }
+                    }
 
-?>
+                    ?>
+                    </body>
+                    </html>
+
+
+                    <?php
+
+                    if(isset($_POST['update'])){
+
+                        $image = $_FILES['image']['name'];
+                        $allowed_extension = array('gif' , 'png' , 'jpeg', 'jpg' , 'PNG' , 'JPEG' , 'JPG' , 'GIF');
+                        $filename = $_FILES ['image']['name'];
+                        $file_extension = pathinfo($filename , PATHINFO_EXTENSION);
+
+                        $student_id = $_POST['student_id'];
+                        if(!in_array($file_extension, $allowed_extension)){
+                            echo "image not added"  ;
+                            exit();
+                        }else{
+                            $update = "UPDATE students SET image = '$image' WHERE student_id = '$student_id'";
+                            $run_update = mysqli_query($conn,$update);
+                            move_uploaded_file($_FILES["image"]["tmp_name"], "student_image/" . $_FILES["image"] ["name"]);
+
+
+                            if($run_update){
+                                echo "image updated"; 
+                                echo "<script>window.location.href='view-students.php' </script>";
+                            }else{
+                                echo "error" . $conn->error; 
+                            }
+                        }
+                    }
+
+
+                    ob_end_flush();
+
+                    ?>
+
+				</div>
+			</main>
+		</div>
+	</div>
+
+<script src="./admin-template/js/app.js"></script>
+<script src="./styles/bootstrap/js/bootstrap.js"></script>
 </body>
 </html>
-
-
-<?php
-
-if(isset($_POST['update'])){
-
-    $image = $_FILES['image']['name'];
-    $allowed_extension = array('gif' , 'png' , 'jpeg', 'jpg' , 'PNG' , 'JPEG' , 'JPG' , 'GIF');
-    $filename = $_FILES ['image']['name'];
-    $file_extension = pathinfo($filename , PATHINFO_EXTENSION);
-
-    $student_id = $_POST['student_id'];
-    if(!in_array($file_extension, $allowed_extension)){
-        echo "image not added"  ;
-        exit();
-    }else{
-        $update = "UPDATE students SET image = '$image' WHERE student_id = '$student_id'";
-        $run_update = mysqli_query($conn,$update);
-        move_uploaded_file($_FILES["image"]["tmp_name"], "student_image/" . $_FILES["image"] ["name"]);
-
-
-        if($run_update){
-            echo "image updated"; 
-            echo "<script>window.location.href='view-students.php' </script>";
-        }else{
-            echo "error" . $conn->error; 
-        }
-    }
-}
-
-
-ob_end_flush();
-
-?>
--- edit-student.php
+++ edit-student.php
@@ -6,132 +6,218 @@ ob_start();
 
 ?>
 
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
+<meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="keywords" content="adminkit, bootstrap, bootstrap 5, admin, dashboard, template, responsive, css, sass, html, theme, front-end, ui kit, web">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link rel="shortcut icon" href="img/icons/icon-48x48.png" />
+    <link rel="canonical" href="https://demo-basic.adminkit.io/" />
+    <link href="admin-template/css/app.css" rel="stylesheet">
+	<link rel="./stylesheet" href="./styles/bootstrap/css/bootstrap.css">
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600&display=swap" rel="stylesheet">
     <title>Document</title>
 </head>
 <body>
+    <div class="wrapper">
+		<nav id="sidebar" class="sidebar js-sidebar">
+			<div class="sidebar-content js-simplebar">
+				<a class="sidebar-brand" href="index.html">
+          <span class="align-middle">Admin</span>
+        </a>
 
+				<ul class="sidebar-nav">
+					<li class="sidebar-header">
+						Pages
+					</li>
 
-    <a href="view-students.php">Back</a>
-    <?php
-    
-    
+					<li class="sidebar-item">
+						<a class="sidebar-link" href="home-admin.php">
+                            <i class="align-middle" data-feather="home"></i> <span class="align-middle">Home</span>
+                         </a>
+					</li>
 
-    //to be continued to 
-    if(isset($_GET['student_id'])){
-        $student_id = $_GET['student_id'];
-        
+					<li class="sidebar-item active">
+						<a class="sidebar-link" href="view-students.php">
+                            <i class="align-middle" data-feather="user"></i> <span class="align-middle">Students</span>
+                        </a>
+					</li>
 
-        $query = "SELECT students.student_id AS student_student_id, students.first_name AS student_first_name, students.last_name AS student_last_name ,students.middle_name AS student_middle_name, students.date_of_birth AS student_date_of_birth, students.gender AS student_gender ,students.room AS student_room,students.house AS student_house,students.street AS student_street,students.subdivision AS student_subdivison,students.barangay AS student_barangay,students.city AS student_city,students.zip AS student_zip,students.grade AS student_grade,students.section AS student_section,students.date_time_created AS student_date_time_created,users.first_name AS user_first_name , users.middle_name AS user_middle_name , users.last_name AS user_last_name, users.room AS user_room, users.house AS user_house , users.street AS user_street, users.subdivision AS user_subdivision, users.barangay AS user_barangay, users.city AS user_city , users.zip AS user_zip , users.image AS user_image , users.student_id AS user_student_id, users.user_id AS user_user_id FROM students LEFT JOIN users
-        ON students.student_id = users.student_id WHERE students.student_id = '$student_id'";
-        $run = mysqli_query($conn,$query);
+					<li class="sidebar-item">
+						<a class="sidebar-link disabled" href="#">
+                            <i class="align-middle" data-feather="star"></i> <span class="align-middle">Nutritionists</span>
+                        </a>
+					</li>
 
-        if(mysqli_num_rows($run) > 0){
-            foreach($run as $row ){
-                ?>
+					<li class="sidebar-item">
+						<a class="sidebar-link" href="add-patient.php">
+                            <i class="align-middle" data-feather="user-plus"></i> <span class="align-middle">Add Patient</span>
+                        </a>
+					</li>
+				
+				</ul>
+				<a class="text-light text-center btn btn-primary" href="logout-admin.php">Logout</a>
+			</div>
+		</nav>
 
-                    <form action="edit-student.php" method="POST" enctype="multipart/form-data">
+		<div class="main">
+			<nav class="navbar navbar-expand navbar-light navbar-bg">
+				<a class="sidebar-toggle js-sidebar-toggle">
+          <i class="hamburger align-self-center"></i>
+        </a>
 
+				<div class="navbar-collapse collapse">
+					<ul class="navbar-nav navbar-align">
+						
+						<li class="nav-item dropdown">
+							<a class="nav-icon dropdown-toggle d-inline-block d-sm-none" href="#" data-bs-toggle="dropdown">
+                                <i class="align-middle" data-feather="settings"></i>
+                            </a>
 
-                        <h2>Personal Info</h2>
-                        <label for="">First Name</label>
-                        <input type="text" name="first_name" value="<?php echo $row ['student_first_name']?>">
+							<a class="nav-link dropdown-toggle d-none d-sm-inline-block" href="#" data-bs-toggle="dropdown" id="dropdownMenuButton1">
+                                <span class="text-dark">ADMIN</span>
+                            </a>
+							<div class="dropdown-menu dropdown-menu-end">
+								<a class="dropdown-item" href="pages-profile.html"><i class="align-middle me-1" data-feather="user"></i> Profile</a>
+								<a class="dropdown-item" href="#"><i class="align-middle me-1" data-feather="pie-chart"></i> Analytics</a>
+								<div class="dropdown-divider"></div>
+								<a class="dropdown-item" href="index.html"><i class="align-middle me-1" data-feather="settings"></i> Settings & Privacy</a>
+								<a class="dropdown-item" href="#"><i class="align-middle me-1" data-feather="help-circle"></i> Help Center</a>
+								<div class="dropdown-divider"></div>
+								<a class="dropdown-item" href="logout-admin.php">Log out</a>
+							</div>
+						</li>
+					</ul>
+				</div>
+			</nav>
 
-                        <br>
+			<main class="content">
+				<div class="container-fluid p-0">
 
-                        <label for="">Middle Name</label>
-                        <input type="text" name="middle_name" value="<?php echo $row ['student_middle_name']?>">
-
-                        <br>
-
-                        <label for="">Last Name</label>
-                        <input type="text" name="last_name" value="<?php echo $row ['student_last_name']?>">
-                        <br>
-
-                        <label for="">Date of Birth</label>
-                        <input type="date" name="date_of_birth" value="<?php echo $row ['student_date_of_birth']?>" id="">
-                        <br>
-
-                        <br>
-
-                        <a href="edit-student-image.php?student_id=<?php echo $row ['student_student_id']?>">Edit Photo</a>
-
-                        <a href="edit-guardian-details.php?student_id=<?php echo $row ['student_student_id']?>">Edit Guardian Details</a>
-
-                        <br>
-                        <h2>Address</h2>
-                        <label for="">Room / Floor / Unit No. & Building Name</label>
-                        <input type="text" name="room" value="<?php echo $row ['student_room'] ?>">
-
-                        <br>
-                        <label for="">House / Lot & Block No.</label>
-                        <input type="text" name="house" value="<?php echo $row ['student_house']?>">
-
-                        <br>
-                        <label for="">Street</label>
-                        <input type="text" name="street" value="<?php echo $row ['student_street']?>">
-
-                        <br>
-                        <label for="">Subdivision</label>
-                        <input type="text" name="subdivision" value="<?php echo $row ['student_subdivison']?>">
-
-
-                        <br>
-                        <label for="">Barangay</label>
-                        <input type="text" name="barangay" value="<?php echo $row ['student_barangay']?>"> 
-
-
-                        <br>
-                        <label for="">City</label>
-                        <input type="text" name="city" value="<?php echo $row ['student_city']?>">
-
-                        
-                        <br>
-                        <label for="">Zip Code</label>
-                        <input type="number" name="zip_code" value="<?php echo $row ['student_zip']?>"> 
-
-                        <input type="hidden" name="gender" value="<?php echo $row ['student_gender']?>">
-                        <br>
-                        
-
-                        <br>
-
-                        <label for="">Grade</label>
-                        <input type="text" name="grade" value="<?php echo $row ['student_grade']?>" readonly>
-                        <select name="grade" id="">
-                            <option value="Grade 1">Grade 1</option>
-                            <option value="Grade 2">Grade 2</option>
-                            <option value="Grade 3">Grade 3</option>
-                            <option value="Grade 4">Grade 4</option>
-                            <option value="Grade 5">Grade 5</option>
-                            <option value="Grade 6">Grade 6</option>
-                        </select>
-
-                        <br>
-                        <label for="">Section</label>
-                        <input type="text" name="section" value="<?php echo $row ['student_section']?>">
-
-                        <input type="hidden" name="date_time_created" value="<?php echo $row ['student_date_time_created']?>">
-
-                        <br>
-                        <input type="submit" name="update" value="Update">
-                        <input type="hidden" name="student_id" value="<?php echo $row ['student_student_id']?>">
-
-                    </form>
-
+                <a href="view-students.php">Back</a>
                 <?php
-            }
-        }
-    }
+                
+                
 
-    ob_end_flush();
-    ?>
+                //to be continued to 
+                if(isset($_GET['student_id'])){
+                    $student_id = $_GET['student_id'];
+                    
+
+                    $query = "SELECT students.student_id AS student_student_id, students.first_name AS student_first_name, students.last_name AS student_last_name ,students.middle_name AS student_middle_name, students.date_of_birth AS student_date_of_birth, students.gender AS student_gender ,students.room AS student_room,students.house AS student_house,students.street AS student_street,students.subdivision AS student_subdivison,students.barangay AS student_barangay,students.city AS student_city,students.zip AS student_zip,students.grade AS student_grade,students.section AS student_section,students.date_time_created AS student_date_time_created,users.first_name AS user_first_name , users.middle_name AS user_middle_name , users.last_name AS user_last_name, users.room AS user_room, users.house AS user_house , users.street AS user_street, users.subdivision AS user_subdivision, users.barangay AS user_barangay, users.city AS user_city , users.zip AS user_zip , users.image AS user_image , users.student_id AS user_student_id, users.user_id AS user_user_id FROM students LEFT JOIN users
+                    ON students.student_id = users.student_id WHERE students.student_id = '$student_id'";
+                    $run = mysqli_query($conn,$query);
+
+                    if(mysqli_num_rows($run) > 0){
+                        foreach($run as $row ){
+                            ?>
+
+                                <form action="edit-student.php" method="POST" enctype="multipart/form-data">
+
+
+                                    <h2>Personal Info</h2>
+                                    <label for="">First Name</label>
+                                    <input type="text" name="first_name" value="<?php echo $row ['student_first_name']?>">
+
+                                    <br>
+
+                                    <label for="">Middle Name</label>
+                                    <input type="text" name="middle_name" value="<?php echo $row ['student_middle_name']?>">
+
+                                    <br>
+
+                                    <label for="">Last Name</label>
+                                    <input type="text" name="last_name" value="<?php echo $row ['student_last_name']?>">
+                                    <br>
+
+                                    <label for="">Date of Birth</label>
+                                    <input type="date" name="date_of_birth" value="<?php echo $row ['student_date_of_birth']?>" id="">
+                                    <br>
+
+                                    <br>
+
+                                    <a href="edit-student-image.php?student_id=<?php echo $row ['student_student_id']?>">Edit Photo</a>
+
+                                    <a href="edit-guardian-details.php?student_id=<?php echo $row ['student_student_id']?>">Edit Guardian Details</a>
+
+                                    <br>
+                                    <h2>Address</h2>
+                                    <label for="">Room / Floor / Unit No. & Building Name</label>
+                                    <input type="text" name="room" value="<?php echo $row ['student_room'] ?>">
+
+                                    <br>
+                                    <label for="">House / Lot & Block No.</label>
+                                    <input type="text" name="house" value="<?php echo $row ['student_house']?>">
+
+                                    <br>
+                                    <label for="">Street</label>
+                                    <input type="text" name="street" value="<?php echo $row ['student_street']?>">
+
+                                    <br>
+                                    <label for="">Subdivision</label>
+                                    <input type="text" name="subdivision" value="<?php echo $row ['student_subdivison']?>">
+
+
+                                    <br>
+                                    <label for="">Barangay</label>
+                                    <input type="text" name="barangay" value="<?php echo $row ['student_barangay']?>"> 
+
+
+                                    <br>
+                                    <label for="">City</label>
+                                    <input type="text" name="city" value="<?php echo $row ['student_city']?>">
+
+                                    
+                                    <br>
+                                    <label for="">Zip Code</label>
+                                    <input type="number" name="zip_code" value="<?php echo $row ['student_zip']?>"> 
+
+                                    <input type="hidden" name="gender" value="<?php echo $row ['student_gender']?>">
+                                    <br>
+                                    
+
+                                    <br>
+
+                                    <label for="">Grade</label>
+                                    <input type="text" name="grade" value="<?php echo $row ['student_grade']?>" readonly>
+                                    <select name="grade" id="">
+                                        <option value="Grade 1">Grade 1</option>
+                                        <option value="Grade 2">Grade 2</option>
+                                        <option value="Grade 3">Grade 3</option>
+                                        <option value="Grade 4">Grade 4</option>
+                                        <option value="Grade 5">Grade 5</option>
+                                        <option value="Grade 6">Grade 6</option>
+                                    </select>
+
+                                    <br>
+                                    <label for="">Section</label>
+                                    <input type="text" name="section" value="<?php echo $row ['student_section']?>">
+
+                                    <input type="hidden" name="date_time_created" value="<?php echo $row ['student_date_time_created']?>">
+
+                                    <br>
+                                    <input type="submit" name="update" value="Update">
+                                    <input type="hidden" name="student_id" value="<?php echo $row ['student_student_id']?>">
+
+                                </form>
+
+                            <?php
+                        }
+                    }
+                }
+
+                ob_end_flush();
+                ?>
+				</div>
+			</main>
+		</div>
+	</div>
+
+<script src="./admin-template/js/app.js"></script>
+<script src="./styles/bootstrap/js/bootstrap.js"></script>
 </body>
 </html>
 
